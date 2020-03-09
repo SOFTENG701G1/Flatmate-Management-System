8d54820faea16564dc7909f5b831640b275bcd33
Bryan <bryanang1998@gmail.com>
2020-03-09 21:14:15 +1300

    Added Key annotation to model classes

--- WebApiBackend/WebApiBackend/Model/Payment.cs
+++ WebApiBackend/WebApiBackend/Model/Payment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,6 +18,7 @@ namespace WebApiBackend.Model
 
     public class Payment
     {
+        [Key]
         public int Id { get; set; }
         public int Amount { get; set; }
         public bool Fixed { get; set; }
