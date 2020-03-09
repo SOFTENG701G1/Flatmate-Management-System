904ec15f2bebad427ac56d88db7ff54878228c55
Richard Ng <42757966+Richardng12@users.noreply.github.com>
2020-03-09 17:40:59 +1300

    Introducing data model classes (#18)
    Merged data model classes

---
+++ WebApiBackend/WebApiBackend/Model/Schedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiBackend.Model
+{
+    public enum ScheduleType
+    {
+        Away
+    }
+
+    public class Schedule
+    {
+        public string UserName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int MyProperty { get; set; }
+        public ScheduleType ScheduleType { get; set; }
+    }
+}
