c4aa90fd5033039f8e1ba57df6978b162a074991
Richard Ng <richardng1998@gmail.com>
2020-03-09 17:19:56 +1300

    Introducing data model classes

---
+++ WebApiBackend/WebApiBackend/Model/Payment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiBackend.Model
+{
+    public enum PaymentType
+    {
+        Rent, Electricity, Water, Internet, Groceries, Other
+    }
+
+    public enum Frequency
+    {
+        OneOff, Weekly, Fortnightly, Monthly
+    }
+
+    public class Payment
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+        public bool Fixed { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public User Users { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public Frequency Frequency { get; set; }
+    }
+}
---
+++ WebApiBackend/WebApiBackend/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiBackend.Model
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string MedicalInformation { get; set; }
+        public string BankAccount { get; set; }
+        public string Password { get; set; }
+    }
+}
