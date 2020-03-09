147f7ccef56d5fad7f379bdd831901bdb0139151
Bryan <bryanang1998@gmail.com>
2020-03-09 21:17:15 +1300

    Configured database context with Fluent API

--- WebApiBackend/WebApiBackend/Model/FlatManagementContext.cs
+++ WebApiBackend/WebApiBackend/Model/FlatManagementContext.cs
@@ -10,7 +10,47 @@ namespace WebApiBackend.Model
     {
         public DbSet<TestModelItem> TestItems { get; set; }
 
+        public DbSet<Flat> Flat { get; set; }
+
+        public DbSet<Payment> Payment { get; set; }
+
+        public DbSet<Schedule> Schdule { get; set; }
+
+        public DbSet<User> User { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite("Data Source=testdb.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Schedule>(entity =>
+            {
+            entity.HasKey(e => new { e.UserName, e.StartDate, e.EndDate, e.ScheduleType });
+
+            entity.Property(e => e.ScheduleType)
+                .HasConversion(
+                    e => e.ToString(),
+                    e => (ScheduleType)Enum.Parse(typeof(ScheduleType), e));
+            });
+
+            modelBuilder.Entity<User>(entity => {
+                entity.HasIndex(e => e.Email)
+                    .IsUnique();
+
+                entity.HasIndex(e => e.PhoneNumber)
+                    .IsUnique();
+            });
+
+            modelBuilder.Entity<Payment>(e => {
+                e.Property(e => e.PaymentType)
+                    .HasConversion(
+                        e => e.ToString(),
+                        e => (PaymentType)Enum.Parse(typeof(PaymentType), e));
+                e.Property(e => e.Frequency)
+                    .HasConversion(
+                        e => e.ToString(),
+                        e => (Frequency)Enum.Parse(typeof(Frequency), e));
+            });
+        }
     }
 }
