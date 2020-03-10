9823d1a8f8cfbf526370e672d934d7d9acecbf67
Bryan <bryanang1998@gmail.com>
2020-03-10 16:59:33 +1300

    Added property fix migration

--- WebApiBackend/WebApiBackend/Migrations/20200310035804_PropertyFixMigration.Designer.cs
+++ WebApiBackend/WebApiBackend/Migrations/20200310035804_PropertyFixMigration.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApiBackend.Model;
 
 namespace WebApiBackend.Migrations
 {
     [DbContext(typeof(FlatManagementContext))]
-    partial class FlatManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20200310035804_PropertyFixMigration")]
+    partial class PropertyFixMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
