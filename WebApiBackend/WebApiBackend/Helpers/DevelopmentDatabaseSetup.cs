056a4e81bb0d698d1e0575a8a19acd96fd3d2778
Hong <hongye1007@gmail.com>
2020-03-10 19:18:31 +1300

    Added userPayment objects into test data to mock many to many relationship

--- WebApiBackend/WebApiBackend/Helpers/DevelopmentDatabaseSetup.cs
+++ WebApiBackend/WebApiBackend/Helpers/DevelopmentDatabaseSetup.cs
@@ -8,32 +8,19 @@ namespace WebApiBackend.Helpers
 {
     public class DevelopmentDatabaseSetup
     {
+        User user1, user2, user3;
+        Payment payment1, payment2;
+        // Payment[NAME]1 is for electricity Payment[NAME]2 is for Rent due to many to many relationship
+        UserPayment userPaymentYin1, userPaymentYin2, userPaymentBryan1,
+            userPaymentBryan2, userPaymentTeresa1, userPaymentTeresa2;
+        Schedule schedule1;
+        Flat flat1;
+
         private readonly FlatManagementContext _database;
 
-        public DevelopmentDatabaseSetup(FlatManagementContext database)
+        public void InitialiseTestDataObjects()
         {
-            _database = database;
-        }
-        public void initialiseUsers()
-        {
-
-        }
-
-        public void SetupDevelopmentDataSet()
-        {
-            User user1 = new User
-            {
-                UserName = "bob101",
-                FirstName = "Bob",
-                LastName = "Sun",
-                DateOfBirth = new DateTime(1998, 05, 16),
-                PhoneNumber = "0218934932",
-                Email = "bobsun@gmail.com",
-                MedicalInformation = "N/A",
-                Password = "bobsthebest303"
-            };
-
-            User user2 = new User
+            user1 = new User
             {
                 UserName = "YinWang",
                 FirstName = "Yin",
@@ -45,7 +32,7 @@ namespace WebApiBackend.Helpers
                 Password = "MustacheMan22"
             };
 
-            User user3 = new User
+            user2 = new User
             {
                 UserName = "TreesAreGreen",
                 FirstName = "Teresa",
@@ -57,19 +44,7 @@ namespace WebApiBackend.Helpers
                 Password = "TreestheBest"
             };
 
-            User user4 = new User
-            {
-                UserName = "ALargeSalmon",
-                FirstName = "Clayton",
-                LastName = "Lan",
-                DateOfBirth = new DateTime(1984, 02, 09),
-                PhoneNumber = "0239876228",
-                Email = "clyanine@hotmail.com",
-                MedicalInformation = "Dementia",
-                Password = "Sickgainz123"
-            };
-
-            User user5 = new User
+            user3 = new User
             {
                 UserName = "BeboBryan",
                 FirstName = "Bryan",
@@ -80,28 +55,77 @@ namespace WebApiBackend.Helpers
                 MedicalInformation = "COVID-19, Extra Chromosome",
                 Password = "StolenGirlfriend123"
             };
-            Payment payment1 = new Payment
+
+            payment1 = new Payment
             {
+                Id = 1,
                 PaymentType = PaymentType.Electricity,
                 Amount = 175,
                 Fixed = false,
                 Frequency = Frequency.Monthly,
                 StartDate = new DateTime(2020, 03, 07),
                 EndDate = new DateTime(2020, 06, 07),
-                Users = new List<User> { user1, user2, user3, user4, user5 },
             };
-            Payment payment2 = new Payment
+            payment2 = new Payment
             {
+                Id = 2,
                 PaymentType = PaymentType.Rent,
                 Amount = 1000,
                 Fixed = true,
                 Frequency = Frequency.Monthly,
                 StartDate = new DateTime(2020, 03, 01),
                 EndDate = new DateTime(2020, 10, 01),
-                Users = new List<User> { user1, user2, user3, user4, user5 },
+            };
+            userPaymentBryan1 = new UserPayment
+            {
+                Payment = payment1,
+                User = user1,
+                UserName = user1.UserName,
+                PaymentId = payment1.Id
+            };
+            userPaymentBryan2 = new UserPayment
+            {
+                Payment = payment2,
+                User = user3,
+                UserName = user3.UserName,
+                PaymentId = payment2.Id
             };
 
-            Schedule schedule1 = new Schedule
+            userPaymentYin1 = new UserPayment
+            {
+                Payment = payment1,
+                User = user1,
+                UserName = user1.UserName,
+                PaymentId = payment1.Id
+            };
+
+            userPaymentYin2 = new UserPayment
+            {
+                Payment = payment2,
+                User = user1,
+                UserName = user1.UserName,
+                PaymentId = payment2.Id
+            };
+
+            userPaymentTeresa1 = new UserPayment
+            {
+                Payment = payment1,
+                User = user2,
+                UserName = user2.UserName,
+                PaymentId = payment1.Id
+            };
+
+            userPaymentTeresa2 = new UserPayment
+            {
+                Payment = payment2,
+                User = user2,
+                UserName = user2.UserName,
+                PaymentId = payment2.Id
+            };
+            payment1.UserPayments = new List<UserPayment> { userPaymentBryan1, userPaymentTeresa1, userPaymentYin1 };
+            payment2.UserPayments = new List<UserPayment> { userPaymentBryan2, userPaymentTeresa2, userPaymentYin2 };
+
+            schedule1 = new Schedule
             {
                 UserName = "BeboBryan",
                 ScheduleType = ScheduleType.Away,
@@ -109,42 +133,36 @@ namespace WebApiBackend.Helpers
                 EndDate = new DateTime(2020, 05, 01)
             };
 
-
-            Flat flat1 = new Flat
+            flat1 = new Flat
             {
                 Address = "50 Symonds Street",
-                Users = new List<User> { user1, user2, user3, user4, user5 },
+                Users = new List<User> { user1, user2, user3 },
                 Schedules = new List<Schedule> { schedule1 },
                 Payments = new List<Payment> { payment1, payment2 }
             };
 
-            _database.Add(
-                user1
-                );
-            _database.Add(
-                user2
-                );
-            _database.Add(
-                user3
-                );
-            _database.Add(
-                user4
-                );
-            _database.Add(
-                user5
-                );
-            _database.Add(
-                flat1
-                );
-            _database.Add(
-                payment1
-                );
-            _database.Add(
-                payment2
-                );
-            _database.Add(
-                schedule1
-                );
+        }
+
+        public DevelopmentDatabaseSetup(FlatManagementContext database)
+        {
+            _database = database;
+        }
+
+        public void SetupDevelopmentDataSet()
+        {
+            _database.Add(user1);
+            _database.Add(user2);
+            _database.Add(user3);
+            _database.Add(payment1);
+            _database.Add(payment2);
+            _database.Add(userPaymentBryan1);
+            _database.Add(userPaymentBryan2);
+            _database.Add(userPaymentTeresa1);
+            _database.Add(userPaymentTeresa2);
+            _database.Add(userPaymentYin1);
+            _database.Add(userPaymentYin2);
+            _database.Add(schedule1);
+            _database.Add(flat1);
 
             _database.SaveChanges();
         }
