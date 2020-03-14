6330d975ee84edf32e4016e6527de6730d2fa476
yfchen12 <chenyeefei90@gmail.com>
2020-03-14 18:38:35 +1300

    Feature/#50 Display members of the users flat

---
+++ WebApiBackend/WebApiBackend/Controllers/FlatsController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiBackend.Dto;
+using WebApiBackend.Model;
+
+namespace WebApiBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlatController : ControllerBase
+    {
+        private readonly FlatManagementContext _context;
+        private static readonly IMapper _MemberMapper;
+
+        //Create mapper using Automapper
+        static FlatController()
+        {
+            var mapperConfigure = new MapperConfiguration(
+                config =>
+                {
+                    config.CreateMap<User, DisplayMemberDTO>();                  
+                }
+
+            );
+            _MemberMapper = mapperConfigure.CreateMapper();
+        }
+
+
+        public FlatController(FlatManagementContext context)
+        {
+            _context = context;
+        }
+
+
+        [AllowAnonymous]
+        [HttpGet("display/{flatId}")]
+        public ActionResult<List<DisplayMemberDTO>> GetMembers(int flatId)
+        {
+            Flat flat = _context.Flat.Where(f => f.Id == flatId).FirstOrDefault();
+            IQueryable members = _context.Entry(flat).Collection(f => f.Users).Query().OrderBy(u => u.FirstName);
+            return _MemberMapper.Map<List<DisplayMemberDTO>>(members);
+        }
+
+    }
+}
---
+++ WebApiBackend/WebApiBackend/Dto/DisplayMemberDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiBackend.Dto
+{
+    public class DisplayMemberDTO
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
