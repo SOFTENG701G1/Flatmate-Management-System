537a0f30a17fa5deb783268f0afce36cee6ae3b9
Jed Robertson <jrob928@aucklanduni.ac.nz>
2020-03-14 18:04:06 +1300

    Form action for login

--- Frontend/frontend/src/Views/Login.js
+++ Frontend/frontend/src/Views/Login.js
@@ -4,14 +4,42 @@ import { Link } from 'react-router-dom';
 import './Login.css';
 
 export default class Login extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      username: undefined,
+      password: undefined
+    }
+
+    this.login = this.login.bind(this);
+    this.bindInput = this.bindInput.bind(this);
+  }
+
+  login (event) {
+    event.preventDefault();
+    console.log(this.state.username);
+    console.log(this.state.password);
+  }
+  
+  bindInput (event) {
+    let target = event.target;
+
+    this.setState({
+      [target.name]: target.value
+    });
+  }
+
   render() {
     return <div className="login-container">
         <div className='login-backdrop'></div>
         <div className='login-icon'></div>
         <h2> Login to your Account </h2>
-        <input type='text' placeholder='Username'/>
-        <input type='text' placeholder='Password'/>
-        <input type='button' value='Login'/>
+        <form action="#" method="POST">
+          <input type='text' name='username' onChange={this.bindInput} placeholder='Username'/>
+          <input type='password' name='password' onChange={this.bindInput} placeholder='Password'/>
+          <input type='submit' value='Login' onClick={this.login}/>
+        </form>
       </div>
   }
 }
