a61f16bf64a2d7a6b912a65639ad69d2f3bd94f2
Jed Robertson <jrob928@aucklanduni.ac.nz>
2020-03-14 18:16:17 +1300

    Implemented login request

---
+++ Frontend/frontend/src/Util/APIRequest.js
@@ -0,0 +1,17 @@
+const apiBaseUrl = "https://localhost:44394/"
+
+export default class APIRequest {
+    static async login(username, password) {
+        let res = await fetch(apiBaseUrl + "api/user/login",
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({ username: username, password: password })
+        });
+
+        return res;
+    }
+}
--- Frontend/frontend/src/Views/Login.js
+++ Frontend/frontend/src/Views/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './Login.css';
+import APIRequest from '../Util/APIRequest';
 
 export default class Login extends React.Component {
   constructor (props) {
@@ -16,10 +17,31 @@ export default class Login extends React.Component {
     this.bindInput = this.bindInput.bind(this);
   }
 
-  login (event) {
+  async login (event) {
     event.preventDefault();
-    console.log(this.state.username);
-    console.log(this.state.password);
+    this.setState({ error: ""});
+    if (!this.state.username || !this.state.password) {
+      this.setState({ error: "Please enter a username and password. "});
+      return;
+    }
+    
+    let loginResult = await APIRequest.login(this.state.username, this.state.password);
+
+    if (loginResult.ok) {
+      console.log(await loginResult.json())
+    } else {
+      switch (loginResult.status) {
+        case 404:
+          this.setState({ error: "There's no such user. "});
+          break;
+        case 403:
+          this.setState({ error: "Invalid password. "});
+          break;
+        default:
+          this.setState({ error: "Unknown error (check your internet). "});
+          break;
+      }
+    }
   }
   
   bindInput (event) {
@@ -38,6 +60,7 @@ export default class Login extends React.Component {
         <form action="#" method="POST">
           <input type='text' name='username' onChange={this.bindInput} placeholder='Username'/>
           <input type='password' name='password' onChange={this.bindInput} placeholder='Password'/>
+          { this.state.error ? <div className='login-error'> { this.state.error } </div> : '' }
           <input type='submit' value='Login' onClick={this.login}/>
         </form>
       </div>
