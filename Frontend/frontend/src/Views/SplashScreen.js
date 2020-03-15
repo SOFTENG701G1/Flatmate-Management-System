c4061f2f4d5a212382fcc57301235b25373f77e4
Mitchell Faulconbridge <mitchellfaulcon@gmail.com>
2020-03-16 07:14:44 +1300

    Splashscreen redirects to app straight away if user is already logged in

--- Frontend/frontend/src/Views/SplashScreen.js
+++ Frontend/frontend/src/Views/SplashScreen.js
@@ -17,6 +17,7 @@ export default class SplashScreen extends React.Component {
   render() {
     return (
       <div className="login-backdrop" onClick={this.start}>
+        { User.getUserState() && <Redirect to="/app/"/> }
         { this.state.redirectTo && <Redirect to={this.state.redirectTo}/> }
         <img src={Logo} className="splash-logo-image"/>
       </div>
