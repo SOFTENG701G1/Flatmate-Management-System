96d66f210f603c62da8df4e62f0d54ef30758a31
Raghav Garg <raghavgarg32@gmail.com>
2020-03-15 21:20:30 +1300

    Feature/#99 add chores to menu (#100)
    * Made chores page accessible through url
    
    * Fixed component chores url referred to
    
    * Added chores option to the left menu and it navigates to chores page when clicked on

---
+++ Frontend/frontend/src/Components/Chores.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import '../App.css';
+
+export default class Chores extends Component {
+    render () {
+        return (
+            <h2>Chores</h2>
+        );
+    }
+}
