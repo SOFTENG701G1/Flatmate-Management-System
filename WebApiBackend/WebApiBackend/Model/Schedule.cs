7993b4c97b02f15d7c39006b256d1e10bde066ce
Hong <hongye1007@gmail.com>
2020-03-15 13:32:14 +1300

    added ids as primary keys

--- WebApiBackend/WebApiBackend/Model/Schedule.cs
+++ WebApiBackend/WebApiBackend/Model/Schedule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,8 @@ namespace WebApiBackend.Model
 
     public class Schedule
     {
+        [Key]
+        public int Id { get; set; }
         public string UserName { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
--- WebApiBackend/WebApiBackend/Model/User.cs
+++ WebApiBackend/WebApiBackend/Model/User.cs
@@ -10,6 +10,7 @@ namespace WebApiBackend.Model
     public class User
     {
         [Key]
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
