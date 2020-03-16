4d7cd00909128ec48f6faed493dcbfd8cb0f96ca
Hong <37763078+HongShi10@users.noreply.github.com>
2020-03-17 11:49:11 +1300

    AutoMapper for DTO and API endpoints for Payments (#115)
    * Started base func for payments api
    
    * Started base func for payments api
    
    * added ids as primary keys
    
    * fixed test data initialisation
    
    * updated database
    
    * modified paymentsrepo
    
    * fixed dependency injection for repository pattern to enable functional payment endpoints
    
    * added Payments controller logic
    
    * removed migration
    
    * automapper nuget package
    
    * Removed package dependency sqlextenstions
    
    * added automapper and DTO for data model classes
    
    * added get payments from flatid
    
    * added api endpoint to delete a user from a payment
    
    * updaed Userpaymentsrepo to delete user from payment
    
    * Created methods to add users to existing payments and creating new payment
    
    * Removed Sqlserver package ref
    
    Co-authored-by: Richard Ng <richardng1998@gmail.com>

---
+++ WebApiBackend/WebApiBackend/EF/PaymentsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiBackend.Model;
+
+namespace WebApiBackend.EF
+{
+    public class PaymentsRepository : EfRepository<Payment,FlatManagementContext>
+    {
+        public PaymentsRepository (FlatManagementContext context) : base(context)
+        {
+
+        }
+        
+    }
+}
+
