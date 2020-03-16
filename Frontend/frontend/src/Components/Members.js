6ae457f2e19ceda16e6681b71e21666de39ed875
style0403 <style2093@hotmail.com>
2020-03-17 11:38:22 +1300

    ability to add username to the member page (not authroising nor searching through db)

--- Frontend/frontend/src/Components/Members.js
+++ Frontend/frontend/src/Components/Members.js
@@ -1,86 +1,51 @@
 import React, {Component} from 'react';
 import '../App.css';
 import './Members.css';
-import { Divider } from '@material-ui/core';
 export default class MembersPage extends Component {
 
     constructor(props){
         super(props)
         this.state ={
             //dummy data
-            userDetails: [{
-                userName: "superman",
-                fullName: "clolly dll",
-                DOB: "03/04/1997",
-                phoneNumber: "021020400203",
-            }, {
-                userName: "batman",
-                fullName: "Daniel all",
-                DOB: "03/12/1998",
-                phoneNumber: "02102242403",
-            }],
+            userNames: []
 
         }
 
     }
     
-    handleSubmit = (event) => {
-        event.preventDefault()
-        const data = this.state
+    addUser(e){
+        e.preventDefault();
+        const {userNames} = this.state;
+        const newUser = this.newUser.value;
         this.setState({
-            [event.target.name]: event.target.value 
-         })
-        console.log("final data ",data)
-
+            userNames: [...this.state.userNames, newUser]
+        })
     }
-    handleInputChange = (event) =>{
-        event.preventDefault()
-    }
+    
     render () {
-        const {userName} = this.state
-        const members = []
-        this.state.userDetails.forEach(
-            users => {
-                members.push(
-                    <MemberProfiles userDetails={users} />
-                )
-            }
-        )
+        const {userNames} = this.state
         return (
         <div>
             <div className='section-header'>
                 Members page
             </div>
-            <div>            
-                <form onSubmit = {this.handleSubmit}>            
-                <input type = 'text' name = 'userName' className = 'Usernamebox' placeholder='Enter Username'/>
-                <button type = 'Submit' className = "button">Add</button>
-                </form>
+                            
+                <form onSubmit = { (e) => {this.addUser(e)}}>    
+                <div>        
+                <input ref = {input => this.newUser = input}type = 'text' name = 'userName' className = 'Usernamebox' placeholder='Enter Username'/>
+                </div>
+                <button type = 'submit' className = "button" >Add</button>
+                </form> 
+            <div>
+            <h4 className = 'currentMember'>Current Members</h4></div>
+                <div>
+                    {userNames.map(user =>{
+                        return <div className ="MemberProfile">{user}</div>
+                            
+                    })
+                }
+                </div>
             </div>
-            <div><h4 className = 'currentMember'>Current Members</h4></div>
-            <div>{members}</div>
-            
-        </div>
         )
     }
-}
-
-function MemberProfiles(props){
-    const userName = props.userDetails.userName;
-    const fullName = props.userDetails.fullName;
-    const DOB = props.userDetails.DOB;
-    const phoneNumber =props.userDetails.phoneNumber;
-    return (
-        <div className="MemberProfile">
-            {userName}
-            <br></br>
-            {fullName}
-            <br></br>
-            DOB: {DOB}
-            <br></br>
-            Phone number: {phoneNumber}
-            
-        </div>
-        
-    )
 }
