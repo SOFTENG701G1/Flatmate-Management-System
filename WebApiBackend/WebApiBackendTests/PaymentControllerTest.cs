1e7ae80acce1f2e67a4197d09c1a751b8507672c
Wendy <wryao64@gmail.com>
2020-03-17 12:42:09 +1300

    set up controller to be used in test - issue with mapper

--- WebApiBackend/WebApiBackendTests/PaymentControllerTest.cs
+++ WebApiBackend/WebApiBackendTests/PaymentControllerTest.cs
@@ -1,7 +1,12 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.Extensions.Options;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using WebApiBackend.Controllers;
+using WebApiBackend.EF;
+using WebApiBackend.Helpers;
 using WebApiBackend.Model;
 using WebApiBackendTests.Helper;
 
@@ -13,17 +18,35 @@ namespace WebApiBackendTests
         private ServiceDependencyResolver _serviceProvider;
         private FlatManagementContext _context;
 
+        private PaymentsRepository _paymentsRepository;
+        private UserPaymentsRepository _userPaymentsRepository;
+        private FlatRepository _flatRepository;
+        private UserRepository _userRepository;
+
+        private PaymentsController _paymentsController;
+
         [SetUp]
         public void Setup()
         {
             _dbSetUpHelper = new DatabaseSetUpHelper();
             _serviceProvider = _dbSetUpHelper.GetServiceDependencyResolver();
             _context = _dbSetUpHelper.GetContext();
+
+            _paymentsRepository = new PaymentsRepository(_context);
+            _userPaymentsRepository = new UserPaymentsRepository(_context);
+            _flatRepository = new FlatRepository(_context);
+            _userRepository = new UserRepository(_context);
+
+            // ISSUE: controller takes in IMapper, but no way of instantiating
+            _paymentsController = new PaymentsController(_paymentsRepository, _userPaymentsRepository, _flatRepository, _userRepository, null);
         }
 
         [Test]
         public void TestAddUserToExistingPayment()
         {
+            // Act
+            
+            // Assert
             Assert.Pass();
         }
 
@@ -52,8 +75,16 @@ namespace WebApiBackendTests
         }
 
         [Test]
-        public void TestGetPaymentsForFlat()
+        public async Task TestGetPaymentsForFlatAsync()
         {
+            //// Arrange
+            //var flatId = 1;
+
+            //// Act
+            //var response = await _paymentsController.GetPaymentsForFlat(flatId);
+
+            //// Assert
+            //Assert.IsNotNull(response);
             Assert.Pass();
         }
 
