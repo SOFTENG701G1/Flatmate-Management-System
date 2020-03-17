5fd225cb549f65c2f66b60a753210045c84c63b3
tomatopaste202 <thomaszhu.nz@gmail.com>
2020-03-17 21:49:52 +1300

    No commit

--- Frontend/frontend/src/App.js
+++ Frontend/frontend/src/App.js
@@ -1,75 +1,15 @@
 import React from 'react';
 import './App.css';
-import Dashboard from './Components/Dashboard';
-import Chores from './Components/Chores';
 import Payments from './Components/Payments';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Navigation from './Components/Navigation';
-import Login from './Views/Login';
-import Register from './Views/Register';
-import Logout from './Views/Logout';
-import SplashScreen from './Views/SplashScreen';
-import HomePage from './Views/HomePage';
-import RequireLogin from './Components/RequireLogin';
-
 export default class App extends React.Component {
-  render() {
+  render () {
     return (
       <div className="App">
-        <div className="container center">
-          <Router>
-            <Route exact path="/">
-              <SplashScreen/>
-            </Route>
-            <Route path="/home">
-              <HomePage/>
-            </Route>
-            <Route path="/login">
-              <Login/>
-            </Route>
-            <Route path="/register">
-              <Register/>
-            </Route>
-            <Route path="/logout">
-              <Logout/>
-            </Route>
-            <Route path="/app/*">
-              <RequireLogin/>
-              <Navigation/>
-              <Switch>
-                <Route path="/app/dashboard">
-                  <Dashboard />
-                </Route>
-                <Route path="/app/chores">
-                  <Chores />
-                </Route>
-                <Route path="/app/payments">
-                  <Payments />
-                </Route>
-                <Route path="/app/members">
-                  <Members />
-                </Route>
-                <Route path="/app/">
-                  <Home />
-                </Route>
-              </Switch>
-            </Route>
-          </Router>
-        </div>
+        <header className="App-header">
+          <Payments></Payments>
+        </header>
       </div>
     );
   }
-}
-/*Router is linked to functions for the time being*/
-function Home() {
-  return <div class='section-header'>Flat Management System</div>;
-}
-
-function Members() {
-  return <div class='section-header'>Members page</div>;
 }
