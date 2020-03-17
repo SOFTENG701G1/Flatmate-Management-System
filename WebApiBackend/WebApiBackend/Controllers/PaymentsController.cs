97affe42276dc01f8c5a93ea688423d7c004abb2
Richard Ng <richardng1998@gmail.com>
2020-03-17 23:52:08 +1300

    removed unused usings and commented model classes

--- WebApiBackend/WebApiBackend/Controllers/PaymentsController.cs
+++ WebApiBackend/WebApiBackend/Controllers/PaymentsController.cs
@@ -4,13 +4,10 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using WebApiBackend.Dto;
 using WebApiBackend.EF;
-using WebApiBackend.Interfaces;
 using WebApiBackend.Model;
 
 namespace WebApiBackend.Controllers
--- WebApiBackend/WebApiBackend/EF/PaymentsRepository.cs
+++ WebApiBackend/WebApiBackend/EF/PaymentsRepository.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using WebApiBackend.Model;
+﻿using WebApiBackend.Model;
 
 namespace WebApiBackend.EF
 {
