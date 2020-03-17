807b0a59cc7e2db0b8e9b06a98f8b0eafd5d8dbf
Hong <hongye1007@gmail.com>
2020-03-17 21:57:13 +1300

    added underscore to efrepo calss

--- WebApiBackend/WebApiBackend/EF/UserPaymentsRepository.cs
+++ WebApiBackend/WebApiBackend/EF/UserPaymentsRepository.cs
@@ -9,23 +9,23 @@ namespace WebApiBackend.EF
     public class UserPaymentsRepository : EfRepository<UserPayment, FlatManagementContext>
     {
 
-        private readonly FlatManagementContext flatManagementContext;
+        private readonly FlatManagementContext _flatManagementContext;
         public UserPaymentsRepository(FlatManagementContext context) : base(context)
         {
-            this.flatManagementContext = context;
+            this._flatManagementContext = context;
         }
 
             // Deletes the Userpayment mapping for a particular user and payment
             public async Task<UserPayment> DeleteUserFromPayment(int userId, int paymentId)
             {
-                var entity = await flatManagementContext.Set<UserPayment>().FindAsync(userId, paymentId);
+                var entity = await _flatManagementContext.Set<UserPayment>().FindAsync(userId, paymentId);
                 if (entity == null)
                 {
                     return entity;
                 }
 
-                flatManagementContext.Set<UserPayment>().Remove(entity);
-                await flatManagementContext.SaveChangesAsync();
+                _flatManagementContext.Set<UserPayment>().Remove(entity);
+                await _flatManagementContext.SaveChangesAsync();
 
                 return entity;
         }
