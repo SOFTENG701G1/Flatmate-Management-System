b1d6c6d50be48f942120976fd4f562396a89d976
Hong <hongye1007@gmail.com>
2020-03-18 15:54:52 +1300

    added DTO and Automapper for chores

--- WebApiBackend/WebApiBackend/Dto/ChoresDTO.cs
+++ WebApiBackend/WebApiBackend/Dto/ChoresDTO.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using WebApiBackend.Interfaces;
-
-namespace WebApiBackend.Model
+﻿ using System;
+namespace WebApiBackend.Dto
 {
-    public class Chores : IEntity
+    public class ChoresDTO
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public ICollection<User> Users { get; set; }
         public string Description { get; set; }
         public DateTime DueDate { get; set; }
         public bool Recurring { get; set; }
