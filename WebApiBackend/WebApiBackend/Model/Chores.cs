227912da9e6b09e6e56ef7e6f8cfe5f996ae4c3b
Hong <hongye1007@gmail.com>
2020-03-18 15:44:49 +1300

    added chores datamodel class

---
+++ WebApiBackend/WebApiBackend/Model/Chores.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApiBackend.Model
+{
+    public class Chores
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<User> Users { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Recurring { get; set; }
+    }
+}
