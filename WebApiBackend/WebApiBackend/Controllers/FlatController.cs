0a5e98328e11437cf55627b1ee6b2363fbde7e39
Wulei Gui <wilbert.gui.nz@gmail.com>
2020-03-18 13:59:42 +1300

    add member interface

--- WebApiBackend/WebApiBackend/Controllers/FlatController.cs
+++ WebApiBackend/WebApiBackend/Controllers/FlatController.cs
@@ -48,5 +48,49 @@ namespace WebApiBackend.Controllers
             return _MemberMapper.Map<List<DisplayMemberDTO>>(members);
         }
 
+        [HttpGet("AddUserToFlat/{FlatId}/{userId}")]
+        [Authorize]
+        public ActionResult<AddUserToFlatDto> AddUserToFlat(int FlatId, int userId)
+        {
+
+            User finduser = _context.User.Find(userId);
+
+            //user not exist
+            if (finduser == null)
+                return new AddUserToFlatDto(FlatId,
+                    userId,
+                    2);
+
+            Flat findflat = _context.Flat.Find(FlatId);
+
+            //wrong flat id
+            if (findflat == null)
+                return new AddUserToFlatDto(FlatId,
+                    userId,
+                    3);
+
+            //user already in this flat 
+            if (findflat.Users.Contains(finduser))
+                return new AddUserToFlatDto(
+                    FlatId,
+                    userId,
+                    4);
+
+            //user has already in other flat  
+            if (finduser.FlatId != null)
+                return new AddUserToFlatDto(
+                    FlatId,
+                    userId,
+                    5);
+
+            findflat.Users.Add(finduser);
+            _context.Flat.Update(findflat);
+            _context.SaveChanges();
+
+            return new AddUserToFlatDto(FlatId, userId, 1);
+
+        }
+
+
     }
 }
