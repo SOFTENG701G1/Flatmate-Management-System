dc2c8fde650ff09f8d41030224b5dcb1afe3228a
Zian Yang Ang <41178640+ZianYangAng@users.noreply.github.com>
2020-03-19 19:24:46 +1300

    Connected the payment to view payment component (#172)

--- Frontend/frontend/src/Components/Payments.js
+++ Frontend/frontend/src/Components/Payments.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import NewPayment from "./NewPayment"
-import ViewPayment from "./ViewPayment"
-import "./Payments.css"
+import NewPayment from "./NewPayment";
+import ViewPayment from "./ViewPayment";
+import "./Payments.css";
 import APIRequest from "../Util/APIRequest";
 
 /*
@@ -10,117 +10,120 @@ import APIRequest from "../Util/APIRequest";
 export default class Payments extends Component {
     constructor() {
         super();
+
         this.state = {
             showView: false,
             show: false,
             payments: [],
             contributionPayments: [],
-            currentPayment: {  
-                PaymentType: "",
-                Amount: 0,
-                StartDate: "",
-                EndDate: "",
-                Frequency: "",
-                Contributors: ["", ""]
-            }
-        }
+            currentPayment: {
+                id: 0,
+                paymentType: 0,
+                amount: 0,
+                startDate: "",
+                endDate: "",
+                frequency: ""
+            },
+            currentContributors: [""]
+        };
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         // Obtain payments associated with the logged in user.
-        await APIRequest.obtainUserPayments().then(
-            data => {
-                this.setState({
-                    payments: data
-                })
-            }
-        )
-            
-        for(let i = 0; i < this.state.payments.length; i++){
-            await APIRequest.obtainPaymentContributors(this.state.payments[i]["id"]).then(
-                usersJSON => {
-                    const listUser = []
-                    for(let j = 0; j < usersJSON.length; j++){
-                        listUser.push(usersJSON[j]["userName"]);
-                    }
-                    this.addUserPayment(listUser)
+        await APIRequest.obtainUserPayments().then(data => {
+            this.setState({
+                payments: data
+            });
+        });
+
+        for (let i = 0; i < this.state.payments.length; i++) {
+            await APIRequest.obtainPaymentContributors(
+                this.state.payments[i]["id"]
+            ).then(usersJSON => {
+                const listUser = [];
+                for (let j = 0; j < usersJSON.length; j++) {
+                    listUser.push(usersJSON[j]["userName"]);
                 }
-            )
-            
+                this.addUserPayment(listUser);
+            });
         }
     }
 
-    addUserPayment = (users) => {
+    addUserPayment = users => {
         let contributionPayments = this.state.contributionPayments;
         contributionPayments.push(users);
         this.setState({
             contributionPayments: contributionPayments
-        })
-    }
+        });
+    };
 
     //Methods for opening new payment component
     _handleOpen = () => {
         this.setState({
             show: true
-        })
-   }
+        });
+    };
 
     _handleClose = () => {
         this.setState({
             show: false
-        })
-    }
+        });
+    };
 
     //Methods for opening view payment component
     _handleOpenView = () => {
         this.setState({
             showView: true
-        })
-    }
+        });
+    };
 
     _handleCloseView = () => {
         this.setState({
             showView: false
-        })
-    }
+        });
+    };
 
     _handleEdit = () => {
         this.setState({
             show: true,
             showView: false
-        })
-    }
+        });
+    };
 
-    _handleTableClick = (payment) => {
-        this.setState({
-            currentPayment: payment
-        }, 
-        () => this._handleOpenView())
-    }
+    _handleTableClick = (payment, contributors) => {
+        this.setState(
+            {
+                currentPayment: payment,
+                currentContributors: contributors
+            },
+            () => this._handleOpenView()
+        );
+    };
 
     render() {
         const fixedPaymentsHtml = [];
         const variablePaymentsHtml = [];
         const payments = this.state.payments;
         const contributorsPayments = this.state.contributionPayments;
-        for(let i = 0; i < this.state.payments.length; i++){
+        for (let i = 0; i < this.state.payments.length; i++) {
             const paymentData = payments[i];
             const contributors = contributorsPayments[i];
-            if(paymentData["fixed"]) fixedPaymentsHtml.push(
-                <PaymentModule 
-                    payment={paymentData} 
-                    contributors={contributors}
-                    onTableClick={this._handleTableClick}
-                />
-            )
-            else variablePaymentsHtml.push(
-                <PaymentModule 
-                    payment={paymentData}  
-                    contributors={contributors}
-                    onTableClick={this._handleTableClick}
-                />
-            )
-            
+            if (paymentData["fixed"])
+                fixedPaymentsHtml.push(
+                    <PaymentModule
+                        payment={paymentData}
+                        contributors={contributors}
+                        onTableClick={this._handleTableClick}
+                    />
+                );
+            else
+                variablePaymentsHtml.push(
+                    <PaymentModule
+                        payment={paymentData}
+                        contributors={contributors}
+                        onTableClick={this._handleTableClick}
+                    />
+                );
         }
 
         return (
@@ -132,48 +135,35 @@ export default class Payments extends Component {
                                 <h2 className="PaymentsTitle">Payments</h2>
                             </span>
                             <span className="NewPaymentButton" onClick={this._handleOpen}>
-                                <button className="NewPaymentButton">
-                                    Add new
-                                </button>
+                                <button className="NewPaymentButton">Add new</button>
                             </span>
-                            <NewPayment 
-                                onClose={this._handleClose} 
-                                show={this.state.show} 
-                            />
+                            <NewPayment onClose={this._handleClose} show={this.state.show} />
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <h4 className="Subtitle">
-                                Fixed
-                            </h4>
+                            <h4 className="Subtitle">Fixed</h4>
                         </td>
                         <td>
-                            <h4 className="Subtitle">
-                                Variable
-                            </h4>
+                            <h4 className="Subtitle">Variable</h4>
                         </td>
                     </tr>
                     <tr>
-                        <td>
-                            {fixedPaymentsHtml}
-                        </td>
-                        <td>
-                            {variablePaymentsHtml}
-                        </td>
-                        <ViewPayment 
-                            onCloseView={this._handleCloseView} 
-                            showView={this.state.showView} 
-                            onEdit={this._handleEdit} 
+                        <td>{fixedPaymentsHtml}</td>
+                        <td>{variablePaymentsHtml}</td>
+                        <ViewPayment
+                            onCloseView={this._handleCloseView}
+                            showView={this.state.showView}
+                            onEdit={this._handleEdit}
                             payment={this.state.currentPayment}
+                            contributors={this.state.currentContributors}
                         />
                     </tr>
                 </table>
             </div>
-        )
+        );
     }
 }
-
 
 /*
     This function JSX element takes in the input:
@@ -181,22 +171,32 @@ export default class Payments extends Component {
     - UserPayment: List of usernames as contributors.
 */
 function PaymentModule(props) {
-    // Map enums stored in backend to list.
-    const paymentTypeEnumList = ["Rent", "Electricity", "Water", "Internet", "Groceries", "Other"];
-    const frequencyEnumList = ["OneOff", "Weekly", "Fortnightly", "Monthly"];
+    const onTableClick = props.onTableClick
 
+    // Map enums stored in backend to list.
+
+    const payment = props.payment
     const paymentType = paymentTypeEnumList[props.payment["paymentType"]];
     const amount = props.payment["amount"];
-    const startDate = props.payment.startDate.slice(0, 10).split("-").join("/");
-    const endDate = props.payment.endDate.slice(0, 10).split("-").join("/");
+    const startDate = props.payment.startDate
+        .slice(0, 10)
+        .split("-")
+        .join("/");
+    const endDate = props.payment.endDate
+        .slice(0, 10)
+        .split("-")
+        .join("/");
     const frequency = frequencyEnumList[props.payment["frequency"]];
 
     //Ensure that Payments Page is rendered even when the list does not exist.
-    const contributors = !props.contributors ? ["Loading..."]: props.contributors; 
+    const contributors = !props.contributors
+        ? ["Loading..."]
+        : props.contributors;
     const contributorsToString = contributors.join(", ");
+
     return (
         <div className="PaymentModule">
-            <table className="PaymentModule">
+            <table className="PaymentModule" onClick={() => onTableClick(payment, contributors)}>
                 <tr>
                     <td className="PaymentModuleDataLeft">
                         <h6 className="PaymentModuleHeader">
@@ -205,20 +205,18 @@ function PaymentModule(props) {
                     </td>
                     <td className="PaymentModuleDataRight">
                         <h6 className="PaymentModuleHeader">
-                            <b>{startDate}-{endDate}</b>
+                            <b>
+                                {startDate}-{endDate}
+                            </b>
                         </h6>
                     </td>
                 </tr>
                 <tr>
                     <td className="PaymentModuleDataLeft">
-                        <h6 className="PaymentModuleData">
-                            Amount: ${amount}
-                        </h6>
+                        <h6 className="PaymentModuleData">Amount: ${amount}</h6>
                     </td>
                     <td className="PaymentModuleDataRight">
-                        <h6 className="PaymentModuleData">
-                            Frequency: {frequency}
-                        </h6>
+                        <h6 className="PaymentModuleData">Frequency: {frequency}</h6>
                     </td>
                 </tr>
                 <tr>
@@ -230,5 +228,15 @@ function PaymentModule(props) {
                 </tr>
             </table>
         </div>
-    )
+    );
 }
+
+export const paymentTypeEnumList = [
+    "Rent",
+    "Electricity",
+    "Water",
+    "Internet",
+    "Groceries",
+    "Other"
+];
+export const frequencyEnumList = ["OneOff", "Weekly", "Fortnightly", "Monthly"];
