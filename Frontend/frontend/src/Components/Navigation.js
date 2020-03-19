53ec2abf018b6eb8e96726917768734c71d5551e
NathanMills1 <43223968+NathanMills1@users.noreply.github.com>
2020-03-19 17:17:19 +1300

    View Full Person Details (#168)
    * Made initial Modal
    
    * Added modal css
    
    * Connected User Details tile to members info
    
    * Tidy css
    
    * Fixed Information to display full name
    
    * Fixed close button so it hides modal.
    
    Previously it was calling both hidemodal and openmodal (Due to being a part of the membertile which had an onclick attribute to open the modal)

--- Frontend/frontend/src/Components/Navigation.js
+++ Frontend/frontend/src/Components/Navigation.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import { Link, Redirect } from 'react-router-dom';
 
 import './Navigation.css';
 import User from '../Util/User';
+import UserDetails from './UserDetails'
 
 export default class Navigation extends React.Component {
+
   render() {
     return <nav className="menu">
       <div className="menu__down">
         <ul className="menu__list">
           <li className="menu__list-item menu__logo"></li>
           <li className="menu__list-item menu__user_detail">
+          <div onClick={this.showModal}>
             <div className='user_profile'> </div>
             <div className='user_profile_name'>
               { User.getUserState() ? User.getUserState().userName : '' }
             </div>
+          </div>
           </li>
           <li className="menu__list-item">
             <Link to="/app/"><a className="menu__link" href="#">Home</a></Link>
