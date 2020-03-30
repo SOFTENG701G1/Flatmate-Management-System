ebf82156299ce647fda230af8caa595bd7961d8f
William Li <williamlixu@gmail.com>
2020-03-30 17:37:50 +1300

    Fix dashboard having no payments

--- Frontend/frontend/src/Components/DashboardPayments.js
+++ Frontend/frontend/src/Components/DashboardPayments.js
@@ -16,6 +16,7 @@ export default class DashboardPayments extends Component {
     async componentDidMount() {
         // Obtain payments associated with the logged in user.
         await APIRequest.obtainUserPayments().then(data => {
+            if(!data) data = [];
             data = data.sort((a, b) => a.startDate.localeCompare(b.startDate)).splice(0, Math.min(4, data.length));
             this.setState({
                 payments: data
@@ -26,6 +27,7 @@ export default class DashboardPayments extends Component {
             await APIRequest.obtainPaymentContributors(
                 this.state.payments[i]["id"]
             ).then(usersJSON => {
+                if(!usersJSON) usersJSON = [];
                 const listUser = [];
                 for (let j = 0; j < usersJSON.length; j++) {
                     listUser.push(usersJSON[j]["userName"]);
@@ -44,13 +46,14 @@ export default class DashboardPayments extends Component {
     };
 
     render () {
-        const paymentComponenets = [];
         const payments = this.state.payments;
+        const paymentComponents = [];
         const contributorsPayments = this.state.contributionPayments;
-        for (let i = 0; i < this.state.payments.length; i++) {
+
+        for (let i = 0; i < payments.length; i++) {
             const paymentData = payments[i];
             const contributors = contributorsPayments[i];
-            paymentComponenets.push(
+            paymentComponents.push(
                 <PaymentModule
                     payment={paymentData}
                     contributors={contributors}
@@ -59,10 +62,16 @@ export default class DashboardPayments extends Component {
             );
         }
 
+        if(paymentComponents.length === 0) {
+            paymentComponents.push(
+                <p>You have no upcoming payments.</p>
+            );
+        }
+
         return (
             <div>
                 <p><b>Upcoming Payments</b></p>
-                {paymentComponenets}
+                {paymentComponents}
             </div>
         );
     }
