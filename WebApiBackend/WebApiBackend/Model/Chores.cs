e241fb4d0b2f85ca54e38fbe5f606f6890c2aa0c
Logan Kenwright <lken274@aucklanduni.ac.nz>
2020-04-09 12:33:03 +1200

    added chores to flat management context

--- WebApiBackend/WebApiBackend/Model/Chores.cs
+++ WebApiBackend/WebApiBackend/Model/Chores.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using WebApiBackend.Interfaces;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApiBackend.Model
 {
     public class Chores : IEntity
     {
+        [Key]
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
