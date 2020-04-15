282e6c8d0dacdb381d8ccc0a8300a7660abc0a9c
Daria <dder725@aucklanduni.ac.nz>
2020-04-15 21:02:28 +1200

    Fixed createPayment method, now is getting some values from the modal too

--- Frontend/frontend/src/Util/APIRequest.js
+++ Frontend/frontend/src/Util/APIRequest.js
@@ -1,243 +1,267 @@
 import User from "./User";
 
-const apiBaseUrl = process.env.REACT_APP_BACKEND_API
+const apiBaseUrl = process.env.REACT_APP_BACKEND_API;
 /**
  * This class provides helper functions for API requests, and provides auth for those requests.
  */
 export default class APIRequest {
-    static async login(username, password) {
-        let res = await fetch(apiBaseUrl + "api/user/login",
-            {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                method: "POST",
-                body: JSON.stringify({ username: username, password: password })
-            });
+  static async login(username, password) {
+    let res = await fetch(apiBaseUrl + "api/user/login", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ username: username, password: password }),
+    });
 
-        return res;
+    return res;
+  }
+
+  // Reset password with the given E-mail and password. It is not using username because the reset link was accessed via user's E-mail
+  static async resetPassword(email, password) {
+    let res = await fetch(apiBaseUrl + "api/user/resetPassword", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ username: email, password: password }),
+    });
+
+    return res;
+  }
+
+  // Validate if user is in the system and send an E-mail for resetting password
+  static async forgotPassword(userOrEmail) {
+    let res = await fetch(apiBaseUrl + "api/user/forgotPassword", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ userOrEmail: userOrEmail }),
+    });
+
+    return res;
+  }
+
+  // Check if the reset token given was created less than 1 hour ago and if it's valid
+  // This needs to be executed prior to page rendering, so an async method is used
+  static checkResetToken(email, resetToken) {
+    let res = fetch(apiBaseUrl + "api/user/resetTokenCheck", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ Email: email, ResetToken: resetToken }),
+    });
+
+    return res;
+  }
+
+  // Gets the auth string, which can be added to header "Authorization" for requests that need auth
+  static async getAuthString() {
+    if (!User.getUserState()) {
+      return undefined;
     }
 
-    // Reset password with the given E-mail and password. It is not using username because the reset link was accessed via user's E-mail
-    static async resetPassword(email, password) {
-        let res = await fetch(apiBaseUrl + "api/user/resetPassword",
-            {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                method: "POST",
-                body: JSON.stringify({ username: email, password: password })
-            });
+    return "Bearer " + User.getUserState().token;
+  }
 
-        return res;
-    }
+  //Checks if the username already exists in the database
+  static async checkNewAccount(username, email) {
+    let res = await fetch(apiBaseUrl + "api/user/check", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ username: username, email: email }),
+    });
 
-    // Validate if user is in the system and send an E-mail for resetting password
-    static async forgotPassword(userOrEmail) {
-        let res = await fetch(apiBaseUrl + "api/user/forgotPassword",
-            {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                method: "POST",
-                body: JSON.stringify({ userOrEmail: userOrEmail })
-            });
+    return res;
+  }
 
-        return res;
-    }
+  //Retrieves the user id of the current user
+  static async getUserID() {
+    let authString = await APIRequest.getAuthString();
+    let res = await fetch(apiBaseUrl + "api/user/getUserID", {
+      header: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: authString,
+      },
+      method: "GET",
+    });
+    return res;
+  }
 
-    // Check if the reset token given was created less than 1 hour ago and if it's valid
-    // This needs to be executed prior to page rendering, so an async method is used
-    static checkResetToken(email, resetToken) {
-        let res = fetch(apiBaseUrl + "api/user/resetTokenCheck",
-            {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                method: "POST",
-                body: JSON.stringify({ Email: email, ResetToken: resetToken })
-            });
+  static async register(
+    username,
+    firstName,
+    lastName,
+    dateOfBirth,
+    phoneNumber,
+    email,
+    medicalInfo,
+    bankAccountNumber,
+    password
+  ) {
+    let res = await fetch(apiBaseUrl + "api/user/register", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        username: username,
+        firstname: firstName,
+        lastname: lastName,
+        dateofbirth: dateOfBirth,
+        phonenumber: phoneNumber,
+        email: email,
+        medicalinformation: medicalInfo,
+        bankAccount: bankAccountNumber,
+        password: password,
+      }),
+    });
 
-        return res;
-    }
+    return res;
+  }
 
-    // Gets the auth string, which can be added to header "Authorization" for requests that need auth
-    static async getAuthString() {
-        if (!User.getUserState()) {
-            return undefined;
-        }
+  static async createNewPayment(
+    amount,
+    startDate,
+    paidTo,
+    endDate,
+    account,
+    frequency,
+    contributorsPending,
+    contributorsPaid,
+    description,
+    listOfIds
+  ) {
+    let authString = await APIRequest.getAuthString();
+    console.log("the ids in the ap are" + listOfIds);
+    let res = await fetch(apiBaseUrl + "api/Payments/Flat", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: authString,
+        userIds: listOfIds,
+      },
+      method: "POST",
+      body: JSON.stringify({
+        amount: amount,
+        startDate: startDate,
+        paidTo: paidTo,
+        endDate: endDate,
+        account: account,
+        frequency: frequency,
+        contributorsPending: contributorsPending,
+        contributorsPaid: contributorsPaid,
+        description: description,
+      }),
+    });
+    console.log(res);
+    return res;
+  }
 
-        return 'Bearer ' + User.getUserState().token;
-    }
+  static async obtainUserPayments() {
+    let authString = await APIRequest.getAuthString();
+    let res = await fetch(apiBaseUrl + "api/Payments/User", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: authString,
+      },
+      method: "GET",
+    }).then((data) => {
+      if (data.ok) return data.json();
+    });
 
-    //Checks if the username already exists in the database
-    static async checkNewAccount(username, email) {
-        let res = await fetch(apiBaseUrl + "api/user/check",
-            {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                method: "POST",
-                body: JSON.stringify({ username: username, email: email })
-            });
+    return res;
+  }
 
-        return res;
-    }
+  static async obtainPaymentContributors(paymentId) {
+    let authString = await APIRequest.getAuthString();
+    let res = await fetch(
+      apiBaseUrl + `api/Payments/Users?paymentId=${paymentId}`,
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: authString,
+        },
+        method: "GET",
+      }
+    ).then((data) => {
+      if (data.ok) return data.json();
+    });
 
-    //Retrieves the user id of the current user
-    static async getUserID() {
-        let authString = await APIRequest.getAuthString();
-        let res = await fetch(apiBaseUrl + "api/user/getUserID",
-            {
-                header: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': authString
-                },
-                method: "GET",
-            });
-        return res;
-    }
+    return res;
+  }
 
-    static async register(username, firstName, lastName, dateOfBirth, phoneNumber,
-        email, medicalInfo, bankAccountNumber, password) {
-        let res = await fetch(apiBaseUrl + "api/user/register",
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify({ username: username, firstname: firstName, lastname: lastName,
-                                    dateofbirth: dateOfBirth, phonenumber: phoneNumber, email: email,
-                                    medicalinformation: medicalInfo, bankAccount: bankAccountNumber, password: password})
-        });
+  static async componentDidMount() {
+    let authString = await APIRequest.getAuthString();
+    const res = await fetch(apiBaseUrl + "api/flat/display", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: authString,
+      },
+      method: "GET",
+    });
+    return res;
+  }
 
-        return res;
-    }
+  //Retrieves the list of flat members in the current users flat
+  static async getFlatMembers() {
+    let authString = await APIRequest.getAuthString();
+    let res = await fetch(apiBaseUrl + "api/flat/getmembers", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: authString,
+      },
+      method: "GET",
+      //mode: "no-cors",
+    });
 
-    static async createNewPayment(amount, startDate, paidTo, endDate, account,
-        frequency, contributorsPending, contributorsPaid, description, listOfIds) {
-        let authString = await APIRequest.getAuthString();
-        let res = await fetch(apiBaseUrl + "api/Payments/Flat",
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization':  authString,
-                'userIds': listOfIds
-            },
-            method: "POST",
-            body: JSON.stringify({ amount: amount, startDate: startDate, paidTo: paidTo,
-                                    endDate: endDate, account: account, frequency: frequency,
-                                    contributorsPending: contributorsPending, contributorsPaid: contributorsPaid, description: description})
-        });
-        console.log(res);
-        return res;
-    }
+    return res;
+  }
 
-    static async obtainUserPayments(){
-        let authString = await APIRequest.getAuthString();
-        let res = await fetch(apiBaseUrl + "api/Payments/User", {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': authString
-            },
-            method: "GET",
-        }).then(
-            data => {
-                if (data.ok) return data.json()
-            }
-        )
+  //Gets the server to create a new flat for the user.
+  static async createNewFlat() {
+    let authString = await APIRequest.getAuthString();
+    let res = await fetch(apiBaseUrl + "api/flat/createFlat", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: authString,
+      },
+      method: "POST",
+    });
 
-        return res;
-    }
+    return res;
+  }
 
-    static async obtainPaymentContributors(paymentId) {
-        let authString = await APIRequest.getAuthString();
-        let res = await fetch(apiBaseUrl + `api/Payments/Users?paymentId=${paymentId}`, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': authString
-            },
-            method: "GET",
-        }).then(
-            data => {
-                if (data.ok) return data.json()
-            }
-        )
-
-        return res;
-    }
-
-    static async componentDidMount() {
-        let authString = await APIRequest.getAuthString();
-        const res = await fetch(apiBaseUrl + "api/flat/display", {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': authString
-            },
-            method: "GET",
-        })
-        return res;
-    }
-
-    //Retrieves the list of flat members in the current users flat
-    static async getFlatMembers() {
-        let authString = await APIRequest.getAuthString();
-        let res = await fetch(apiBaseUrl + "api/flat/getmembers",
-            {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': authString
-                },
-                method: "GET",
-                //mode: "no-cors",
-
-            });
-
-        return res
-    }
-
-    //Gets the server to create a new flat for the user.
-    static async createNewFlat() {
-        let authString = await APIRequest.getAuthString();
-        let res = await fetch(apiBaseUrl + "api/flat/createFlat",
-            {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': authString
-                },
-                method: "POST",
-                
-            });
-
-
-        return res
-    }
-
-    // Get ids of specified users
-    static async getUserIdsByUsername(UserID){
-        let res = await fetch(apiBaseUrl + "api/User/getUsersIds",
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            method: "POST",
-            body: JSON.stringify({UserID: UserID})
-        });
-    return res
-    }
-
+  // Get ids of specified users
+  static async getUserIdsByUsername(UserID) {
+    let res = await fetch(apiBaseUrl + "api/User/getUsersIds", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ UserID: UserID }),
+    }).then((data) => {
+      if (data.ok){
+       return data.json();
+      }
+    });
+    return res;
+  }
 }
